7b237d3e7827eb709d079744fbe430ecee544f02
bbrewington <brent.brewington@gmail.com>
2015-02-15 18:59:27 -0500

    Create Plot_2D_2lines.R

---
+++ Plot_2D_2lines.R
@@ -0,0 +1,11 @@
+# This is an example of plotting 2 lines on the same graph using the "Plot" function
+
+# Code:
+y1 <- c(1,8,27,64)
+y2 <- c(64,27,8,1)
+plot(y1,type="l",col="red")
+lines(y2,col="green")
+
+# Here is a picture of the output: http://i.imgur.com/Js9yLsR.png
+
+# Reference: http://stackoverflow.com/questions/2564258/plot-2-graphs-in-same-plot-in-r
