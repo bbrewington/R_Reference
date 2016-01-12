b56b3bc7012a1ccfede5e607f7b82f08325a6cde
Brent Brewington <brent.brewington@gmail.com>
2016-01-11 22:39:28 -0500

    Update template_rvest.R

--- template_rvest.R
+++ template_rvest.R
@@ -3,4 +3,4 @@ page <- "fill in website here"
 page.html <- read_html(page)
 
 # Extract element with html_text()
-page %>% html_node("ENTER CSS SELECTOR HERE") %>% html_text()
+page.html %>% html_node("ENTER CSS SELECTOR HERE") %>% html_text()
