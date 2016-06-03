280f8f85e6c4fe1c969aab59c7ee95bb7440b08e
Brent Brewington <brent.brewington@gmail.com>
2016-06-02 21:10:26 -0400

    Create template_httr.R

---
+++ template_httr.R
@@ -0,0 +1,19 @@
+library(httr)
+
+url <- "http://httpbin.org/get"
+
+# GET regular call
+r <- GET(url)
+
+# GET regular call
+r <- stop_for_status(GET(url))
+
+# GET call w/ headers 
+r <- GET(url, add_headers(a = 1, b = 2))
+
+# GET call w/ query
+r <- GET("http://google.com/", path = "search", query = list(q = "ham"))
+
+headers(r)
+content(r)
+status_code(r)
